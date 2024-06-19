3b5f567e0409d48e6e0aaad0b6dcbc0b7224e8d8
abratreus <abratreus.lopes@gmail.com>
2024-06-18 21:20:16 -0300

    resolvido
    problema resolvido e começo do cadastro

--- Pagina Home/js/app.js
+++ Pagina Home/js/app.js
@@ -1,3 +1,24 @@
-var scrollSpy = new bootstrap.ScrollSpy(document.body, {
-    target: '#navbar-example'
-  })
+let btn = document.getElementById("button")
+let div = document.getElementById("perfil")
+
+function cadastrar(){
+  let nome = document.getElementById("username")
+  let email = document.getElementById("email")
+  if (nome==""||" "||nome.length<=3||IsEmail(email)==false){
+    alert("Escreva um Nome ou um Email válido")
+  }
+}
+function inprimir(){
+
+}
+function error(){
+
+}
+function IsEmail(email){
+  let exclude=/[^@-.w]|^[_@.-]|[._-]{2}|[@.]{2}|(@)[^@]*1/;
+  let check=/@[w-]+./;
+  let checkend=/.[a-zA-Z]{2,3}$/;
+  if(((email.search(exclude) != -1)||(email.search(check)) == -1)||(email.search(checkend) == -1)){return false;}
+  else {return true;}
+}
+btn.addEventListener('click', cadastrar)
